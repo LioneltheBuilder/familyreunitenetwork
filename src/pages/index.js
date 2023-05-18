668c8e4c1f0004c446b45aef5daf6b27c067bda1
Lionelthebuilder <younglionpics@gmail.com>
2023-05-18 12:15:59 -0400

    added buy book section

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,27 @@ import {
   Page,
   ProjectsSection,
   Seo,
+  Section, 
+  Animation 
 } from "gatsby-theme-portfolio-minimal";
-
+const buyBookUrl = "https://www.amazon.com/gp/aw/d/B0976M6MF5/ref=tmm_pap_swatch_0?ie=UTF8&qid=1624368659&sr=8-3";
 export default function IndexPage() {
   return (
     <>
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <ArticlesSection sectionId="articles" heading="Latest Blogs" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
+        <Animation type="fadeUp"> 
+                <Section heading="This is a brand new section.">
+                  <h2>Book Title</h2>
+      <p>Description of the book goes here.</p>
+      <a href={buyBookUrl} target="_blank" rel="noopener noreferrer">
+        Buy Now
+      </a>
+                </Section>
+            </Animation>
         <InterestsSection sectionId="details" heading="Details" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
         <ContactSection sectionId="github" heading="Issues?" />
